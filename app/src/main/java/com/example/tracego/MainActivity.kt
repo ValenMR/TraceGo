6fab6cff5563a2e2f427ff4f9460372470d48e7e
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2025-05-10 23:24:04 -0500

    feat: add color schema

--- app/src/main/java/com/example/tracego/MainActivity.kt
+++ app/src/main/java/com/example/tracego/MainActivity.kt
@@ -1,29 +1,79 @@
 package com.example.tracego
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.tracego.ui.theme.TraceGoTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             TraceGoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    Column {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth(1f)
+                                .height(50.dp)
+                                .background(MaterialTheme.colorScheme.primary),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text(
+                                text = "Primary color",
+                                modifier = Modifier
+                            )
+                        }
+
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth(1f)
+                                .height(50.dp)
+                                .background(MaterialTheme.colorScheme.secondary),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text(
+                                text = "Secondary color",
+                                modifier = Modifier
+                            )
+                        }
+
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth(1f)
+                                .height(50.dp)
+                                .background(MaterialTheme.colorScheme.tertiary),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text(
+                                text = "Tertiary color",
+                                modifier = Modifier
+                            )
+                        }
+                    }
                 }
             }
         }
@@ -32,16 +82,5 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TraceGoTheme {
-        Greeting("Android")
-    }
 }
